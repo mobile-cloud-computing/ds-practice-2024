39c1e27e0c34b43104c011f65ebbd6dc61187a9c
Robin Wersich <wersich@ut.ee>
2024-02-23 21:55:17 +0200

    Remove stub generation container after usage

--- utils/pb/generate_stubs_dockerized.sh
+++ utils/pb/generate_stubs_dockerized.sh
@@ -3,4 +3,4 @@ WORKING_DIR=$(dirname "$0")
 cd "$WORKING_DIR"
 
 docker build -t pb-stub-generation "$(pwd)"
-docker run -v "$(pwd):/pb" pb-stub-generation
+docker run -v "$(pwd):/pb" --rm pb-stub-generation
