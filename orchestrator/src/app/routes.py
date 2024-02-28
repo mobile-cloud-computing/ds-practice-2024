cf405fed419ed4bbcafec5a3ca8fd6262c88f571
eilyre <sander.kuusemets@ut.ee>
2024-02-28 21:19:38 +0200

    Asynchronous gRPC requests working.

--- orchestrator/src/app/routes.py
+++ orchestrator/src/app/routes.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify
+import asyncio
 
 from .schemas.checkout import CheckoutSchema
 from marshmallow import ValidationError
@@ -25,7 +26,7 @@ def init_routes(app):
 
     # Quick test for this: curl localhost:8081/checkout -X POST -H 'Content-Type: application/json' -H 'Referer: http://localhost:8080/' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data '{"user":{"name":"Priit","contact":"Asd xdc"},"creditCard":{"number":"5105105105105100","expirationDate":"12/26","cvv":"123"},"userComment":"Plz dont charge","items":[{"name":"Learning Python","quantity":1}],"discountCode":"#123","shippingMethod":"Snail","giftMessage":"","billingAddress":{"street":"Narva mnt 18u","city":"Tartu","state":"Tartumaa","zip":"51011","country":"Estonia"},"giftWrapping":false,"termsAndConditionsAccepted":true,"notificationPreferences":["email"],"device":{"type":"Smartphone","model":"Samsung Galaxy S10","os":"Android 10.0.0"},"browser":{"name":"Chrome","version":"85.0.4183.127"},"appVersion":"3.0.0","screenResolution":"1440x3040","referrer":"https://www.google.com","deviceLanguage":"en-US"}'
     @app.route('/checkout', methods=['POST'])
-    def checkout():
+    async def checkout():
         """
         Responds with a JSON object containing the order ID, status, and suggested books.
         """
@@ -44,24 +45,27 @@ def init_routes(app):
         try:
             data = schema.load(request.get_json())
 
-        except ValidationError as e:
-            response = {"code": "400", "message": "Invalid request parameters."}
-            response_code = 400
+        except ValidationError:
+            return jsonify({"code": "400", "message": "Invalid request parameters."}), 400
 
-            return jsonify(response), response_code
+        fraud_detection_task = asyncio.create_task(fraud(creditcard=data['creditCard']))
+        verify_transaction_task = asyncio.create_task(verify_transaction(creditcard=data['creditCard']))
+        # suggestions_task = asyncio.create_task(call_suggestions_service())
 
-        # if fraud(creditcard=data['creditCard']):
-        #     response = order_status_response
-        #     response_code = 200
-        # else:
-        #     response = {"code": "400", "message": "Fraud detected."}
-        #     response_code = 400
+        # Await both tasks (i.e., the gRPC calls)
+        fraud_result, verification_result = await asyncio.gather(fraud_detection_task, verify_transaction_task)
 
-        if verify_transaction(creditcard=data['creditCard']):
-            response = order_status_response
-            response_code = 200
-        else:
-            response = {"code": "400", "message": "Transaction credentials are not valid."}
-            response_code = 400
+        result = {
+            "fraud": fraud_result,
+            "verification": verification_result
+        }
 
-        return jsonify(response), response_code
+        print(result)
+
+        if not result['fraud']:
+            return jsonify({"code": "400", "message": "Fraud detected."}), 400
+
+        if not result['verification']:
+            return jsonify({"code": "400", "message": "Transaction credentials are not valid."}), 400
+
+        return jsonify(order_status_response), 200
